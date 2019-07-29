2346b44046746f1781d5e5c3696aff62d3d1b9e8
Eric <eric.a.champagne@verizon.com>
2019-07-29 15:24:35 -0400

    Extend/append

---
+++ listmethods/listmeth02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+protoa = ["ssh", "http", "https"]
+print(proto)
+proto.append('dns') #this will add d,n,s to the end of list
+protoa.append('dns') #this will add d,n,s to protoa list
+print(proto)
+proto2 = [22, 80, 443, 53] #list of common ports
+proto.extend(proto2) #pass proto2 as an extend method
+print(proto)
+protoa.extend(proto2)
+print(protoa)
